373ef93d574bf9631ac05ed1f3f4488214ccc69b
willians ccallo <mayhixccallo@gmail.com>
2023-02-10 16:04:03 -0500

    cahnge ruoter backend

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,3 @@
 // export const API_URL = 'http://127.0.0.1:5000/';
-export const API_URL = 'https://frozen-tor-02595.herokuapp.com';
+export const API_URL = 'https://backendbravaso-production.up.railway.app';
 // 'https://frozen-tor-02595.herokuapp.com';
